9f85aeade441969a3cc9ed1f97aa9b47a23bf550
lrdaftkhar <lrdaftkhar@gmail.com>
2024-01-20 01:04:21 +0330

    config implemenation part 01

--- vgit.c
+++ vgit.c
@@ -3,22 +3,74 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int init();
+// functions prototypes
+int init(void);
 int doesFolderExist(char *);
+void MainSettings(void);
+
+// constant values
+const char settingsFolder[]="C:\\Users\\admin\\Desktop\\settings";
+const char settingsFilePath[]="C:\\Users\\admin\\Desktop\\settings\\settings.txt";
+const char projectsFilePath[]="C:\\Users\\admin\\Desktop\\settings\\projects.txt";
+
+// global variables
+FILE * file;
+char workingDirectory[1024];
+
+
 
 int main(int argc, char const *argv[])
 {
+
+    getcwd(workingDirectory,sizeof(workingDirectory));
+    MainSettings();
+
     if (strcmp(argv[0], "vgit") == 0 && argc == 1)
     {
         printf("\tvgit version 1.0.0");
     }
+    else if(strcmp(argv[0],"vgit")==0 &&strcmp(argv[1],"config")==0 && strcmp(argv[2],"--global")==0){
+        if (strcmp(argv[3],"user.name")==0)
+        {
+
+        }else if(strcmp(argv[3],"user.email")==0){
+
+        }
+        
+    }
     else if (strcmp(argv[0], "vgit") == 0 && strcmp(argv[1], "init") == 0)
     {
-        init(doesFolderExist);
+        init();
     }
 
     return 0;
 }
+
+// vgit main settings
+void MainSettings(){
+    struct stat st;
+
+    if (stat(settingsFolder, &st) == 0 && S_ISDIR(st.st_mode))
+        {
+            // if "settings" folder exists do nothing
+        }
+    else{
+        
+        mkdir(settingsFolder);
+
+        // creating settings.txt
+        chdir(settingsFolder);
+        file =fopen(settingsFilePath,"w");
+        fprintf(file,"");
+        fclose(file);
+        
+        file =fopen(projectsFilePath,"w");
+        fprintf(file,"");
+        fclose(file);
+        chdir(workingDirectory);
+    }
+}
+
 
 // init function implementation
 int init()
