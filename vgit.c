d82670954b1c766aa078660e8b07c636dc6c42d3
lrdaftkhar <lrdaftkhar@gmail.com>
2024-01-20 00:46:15 +0330

    init implemented

--- vgit.c
+++ vgit.c
@@ -1,7 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int init();
+int doesFolderExist(char *);
 
 int main(int argc, char const *argv[])
 {
+    if (strcmp(argv[0], "vgit") == 0 && argc == 1)
+    {
+        printf("\tvgit version 1.0.0");
+    }
+    else if (strcmp(argv[0], "vgit") == 0 && strcmp(argv[1], "init") == 0)
+    {
+        init(doesFolderExist);
+    }
 
     return 0;
+}
+
+// init function implementation
+int init()
+{
+
+    char currentDir[1024];
+    getcwd(currentDir, 1024);
+
+    int vgitExistence = doesFolderExist(".vgit");
+
+    if (vgitExistence == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        chdir(currentDir);
+        mkdir(".vgit");
+        system("attrib +H .vgit");
+        printf("Initialized empty Git repository successfully :)");
+    }
+}
+
+int doesFolderExist(char *folderName)
+{
+    struct stat st;
+
+    char currentDir[1024];
+    getcwd(currentDir, sizeof(currentDir));
+
+    char folderPath[1024];
+    while (1)
+    {
+        // Check if the folder exists in the current directory
+        sprintf(folderPath, "%s\\%s", currentDir, folderName);
+        if (stat(folderPath, &st) == 0 && S_ISDIR(st.st_mode))
+        {
+            printf("cannot create directory '.vgit': File exists\n");
+            return 1;
+        }
+
+        // Move to the parent directory
+        chdir("..");
+
+        getcwd(currentDir, sizeof(currentDir));
+
+        // Check if we've reached the root directory
+        if (strcmp(currentDir, "C:\\") == 0 || strcmp(currentDir, "D:\\") == 0 || strcmp(currentDir, "F:\\"))
+        {
+            sprintf(folderPath, "%s\\%s", currentDir, folderName);
+            if (stat(folderPath, &st) == 0 && S_ISDIR(st.st_mode))
+            {
+                printf("cannot create directory '.vgit': Folder exists\n");
+                return 1;
+            }
+            return 0;
+        }
+    }
 }
